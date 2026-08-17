e8706f60d1d6de582bf41fc35b4db69416807c8d
dyefim <yefimchuk.dmytro@gmail.com>
2026-08-17 23:06:13 +0300

    feat(database): add init script

---
+++ scripts/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS todo_list (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    done BOOLEAN DEFAULT false
+);
+
+INSERT INTO todo_list (title, done) 
+VALUES ('Setup Docker + PostgreSQL', true)
+ON CONFLICT DO NOTHING;
