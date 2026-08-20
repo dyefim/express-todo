bb7645ab7e9f7e092cc86f908ffb0b79df5c0220
dyefim <yefimchuk.dmytro@gmail.com>
2026-08-20 21:02:21 +0300

    refactor(db): correct variable name from PORT to DB_PORT for clarity

--- db.js
+++ db.js
@@ -3,13 +3,13 @@ const pgp = require("pg-promise")(/* options */);
 process.loadEnvFile();
 
 const HOST = "localhost";
-const PORT = process.env.PORT || 5432;
+const DB_PORT = process.env.DB_PORT || 5432;
 const username = process.env.POSTGRES_USER;
 const password = process.env.POSTGRES_PASSWORD;
 const database = process.env.POSTGRES_DB;
 
 const db = pgp(
-  `postgres://${username}${password ? `:${password}` : ""}@${HOST}:${PORT}/${database}`,
+  `postgres://${username}${password ? `:${password}` : ""}@${HOST}:${DB_PORT}/${database}`,
 );
 
 module.exports = db;
