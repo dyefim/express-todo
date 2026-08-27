71563bef4d1e050eb6f5c996606c7181bd7deab3
dyefim <yefimchuk.dmytro@gmail.com>
2026-08-27 20:38:33 +0300

    chore: change debug log message

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.listen(port, () => {
     fs.writeFileSync("logs/operations.log", "");
   }
 
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
