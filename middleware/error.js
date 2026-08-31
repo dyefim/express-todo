5a865bb858c0d008e11ce6e949d16df18eada286
dyefim <yefimchuk.dmytro@gmail.com>
2026-09-01 00:26:59 +0300

    fix(error): change status code for duplicate entry response

--- middleware/error.js
+++ middleware/error.js
@@ -2,7 +2,7 @@ const errorHandler = (err, req, res, next) => {
   console.error(err);
 
   if (err.code === "23505") {
-    return res.status(400).json({ message: "Duplicate entry" });
+    return res.status(409).json({ message: "Duplicate entry" });
   }
 
   return res.status(500).json({ message: "Internal Server Error" });
